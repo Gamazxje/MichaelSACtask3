335c51d3555e6741ad1bb51c515ec2e9ab780dab
Gamazxje <102113682+Gamazxje@users.noreply.github.com>
2022-03-30 14:14:29 +1100

    Add files via upload

---
+++ MichaelSACU301task3/MichaelSACU301task3/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MichaelSACU301task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbFilter = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dgvSales = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(240, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter by:";
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cbFilter.FormattingEnabled = true;
+            this.cbFilter.Items.AddRange(new object[] {
+            "Subject",
+            "Rating",
+            "TextBook"});
+            this.cbFilter.Location = new System.Drawing.Point(380, 131);
+            this.cbFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(156, 33);
+            this.cbFilter.TabIndex = 1;
+            this.cbFilter.SelectedIndexChanged += new System.EventHandler(this.cbFilter_SelectedIndexChanged);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtSearch.Location = new System.Drawing.Point(646, 131);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(146, 31);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // dgvSales
+            // 
+            this.dgvSales.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dgvSales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSales.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvSales.Location = new System.Drawing.Point(80, 279);
+            this.dgvSales.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvSales.Name = "dgvSales";
+            this.dgvSales.RowHeadersWidth = 82;
+            this.dgvSales.RowTemplate.Height = 33;
+            this.dgvSales.Size = new System.Drawing.Size(1242, 427);
+            this.dgvSales.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Image = global::MichaelSACU301task3.Properties.Resources.av6584c34aabb39f00a10;
+            this.pictureBox1.Location = new System.Drawing.Point(604, 131);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1372, 782);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgvSales);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cbFilter);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFilter;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvSales;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MichaelSACU301task3/MichaelSACU301task3/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MichaelSACU301task3
+{
+    
+    public partial class Form1 : Form
+    {
+        List<Sales> sales = new List<Sales>();
+        BindingSource bs = new BindingSource();
+        string filter;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCSV();
+            bs.DataSource = sales;
+            dgvSales.DataSource = bs;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadCSV()
+        {
+            //display the data
+            string filePath = @"c:\Users\demo\Task3_Shop_Data.csv";
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filePath).ToList();
+            foreach (string line in lines)
+            {
+                List<string> items = line.Split(',').ToList();
+                Sales s = new Sales();
+                s.TextBook = items[0];
+                s.Subject = items[1];
+                s.Seller = items[2];
+                s.Purchaser = items[3];
+                s.purchasedPrice = float.Parse(items[4]);
+                s.SalePrice = items[5];
+                s.Rating = items[6];
+                sales.Add(s);
+            }
+        }
+        ////i tried my best on the selection sort but i can't figure it out so i just put my coding in just in case there can be some points i can gain from atleast trying
+        //private List<int> SelectionSort(List<int> Rating)
+        //{
+        //  for (int i = 0; i < Rating.Count  -1; i++)
+        //   {
+        //       int temp = Rating[i];
+        //       int min_index = 0;
+        //      int min_value = 9999;
+        //       for (int j = i; j < Rating.Count; j++)
+        //      {
+        //          if (Rating[j] < min_value)
+        //          {
+        //              min_index = j;
+        //              min_value = Rating[j];
+        //          }
+        //     }
+        //      Rating[i] = min_value;
+        //      Rating[min_index] = temp;
+        //   }
+        //    return Rating;
+        //  }
+        private List<Sales> Search(string target, string filter)
+        {
+            //search function for Rating,Subject,TextBook
+            List<Sales> results = new List<Sales>();
+            foreach(Sales s in sales)
+            {
+                if(filter == "Rating")
+                {
+                    if(s.Rating.ToLower() == target.ToLower()) results.Add(s);
+                }
+                if (filter =="Subject")
+                {
+                    if (s.Subject.ToLower().Contains(target.ToLower())) results.Add(s);
+                }
+                if(filter == "TextBook")
+                {
+                    if (s.TextBook.ToLower().Contains(target.ToLower()))results.Add(s);
+                }
+            }
+            return results;
+        }
+
+        private void cbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filter = cbFilter.Text;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            //search box
+            List<Sales> r = Search(txtSearch.Text, filter);
+            bs.DataSource = r;
+            dgvSales.DataSource = bs;
+            bs.ResetBindings(false);
+        }
+        
+        //private void btnSort_Click(object sender, EventArgs e)
+        //{
+          //sales = SelectionSort(sales);
+            //bs.DataSource = sales;
+            //dgvSales.DataSource = bs;
+          //  bs.ResetBindings(false);
+        //}
+        //i tried my best on the selection sort but i can't figure it out so i just put my coding in just in case there can be some points i can gain from atleast trying
+        //i also have to delete the selection sort button on my design or else everything will crash :)   
+    }
+}
